16b6c80e9a81f5c32a144d58b65cff52fb3a6800
NoelleWeasley <NoelleWeasley@NoelleWeasley.local>
2022-12-01 17:32:16 -0500

    day_1: code further simplified with new read file function

--- day_1/main.py
+++ day_1/main.py
@@ -1,29 +1,21 @@
 # Day 1 of Advent of Code
 
-def calories_tracker(): # I swear this function is forcing me to go to the gym...
-    input_file = open("elf_calories.txt", "r")
-    max_calories = 0
-    current_elf_calories = 0
-    elf_record = list()
+import ReadFileFunctions as RFF
 
-    single_calorie = input_file.readline()
-    while single_calorie:
-        if single_calorie != "\n":
-            single_calorie = int(single_calorie)
-            current_elf_calories += single_calorie
-        else:
-            if max_calories < current_elf_calories:
-                max_calories = current_elf_calories
-            elf_record.append(current_elf_calories)
-            current_elf_calories = 0
 
-        single_calorie = input_file.readline()
+def calories_tracker():
+    # I swear this function is forcing me to go to the gym...
+    calories_profile = RFF.read_file_double_new_line("elf_calories.txt")
 
-    return max_calories, sorted(elf_record)
+    max_calories = max(map(lambda x: sum(map(int, x)), calories_profile))
+    top_three_calories = sum(sorted(map(lambda x: sum(map(int, x)), calories_profile))[-3:])
+
+    return max_calories, top_three_calories
 
 
 if __name__ == '__main__':
     way_too_much_food, all_the_food = calories_tracker()
-    top_three_calories = sum(all_the_food[-3:])
     print(way_too_much_food)
-    print(top_three_calories)
+    print(all_the_food)
+
+
