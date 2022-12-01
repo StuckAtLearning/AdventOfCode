c7903e627bc398680ad702443b42fee95054929c
NoelleWeasley <NoelleWeasley@NoelleWeasley.local>
2022-12-01 17:34:08 -0500

    day_2: test commit

---
+++ day_2/main.py
@@ -0,0 +1,8 @@
+def test_function(test_string):
+    print(test_string)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test_function("it is day 2")
+
