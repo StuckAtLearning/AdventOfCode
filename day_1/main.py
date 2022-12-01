eb225f95309c97d1d326e0973526b09c5abca453
NoelleWeasley <NoelleWeasley@NoelleWeasley.local>
2022-12-01 00:29:41 -0500

    day_1: elf calories solution

--- day_1/main.py
+++ day_1/main.py
@@ -1,9 +1,29 @@
 # Day 1 of Advent of Code
 
-def some_function(an_input):
-    print(an_input)
+def calories_tracker(): # I swear this function is forcing me to go to the gym...
+    input_file = open("elf_calories.txt", "r")
+    max_calories = 0
+    current_elf_calories = 0
+    elf_record = list()
+
+    single_calorie = input_file.readline()
+    while single_calorie:
+        if single_calorie != "\n":
+            single_calorie = int(single_calorie)
+            current_elf_calories += single_calorie
+        else:
+            if max_calories < current_elf_calories:
+                max_calories = current_elf_calories
+            elf_record.append(current_elf_calories)
+            current_elf_calories = 0
+
+        single_calorie = input_file.readline()
+
+    return max_calories, sorted(elf_record)
 
 
 if __name__ == '__main__':
-    some_function('merry xmas')
-
+    way_too_much_food, all_the_food = calories_tracker()
+    top_three_calories = sum(all_the_food[-3:])
+    print(way_too_much_food)
+    print(top_three_calories)
