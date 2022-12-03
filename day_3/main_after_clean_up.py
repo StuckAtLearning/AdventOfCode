3e6e07694e8f540714f377ee72dba031ed7e2a9a
NoelleWeasley <NoelleWeasley@NoelleWeasley.local>
2022-12-03 16:05:34 -0500

    day_3: clean-up version comments added

---
+++ day_3/main_after_clean_up.py
@@ -0,0 +1,37 @@
+import ReadFileFunctions as RFF
+import HelperFunctions as HF
+
+
+def get_priority_sum(single_char: str) -> int:
+    priority_sum = 0
+    priority_sum += ord(single_char) - 38 if ord(single_char) < 97 else ord(single_char) - 96
+    return priority_sum
+
+
+def calculate_priority_sum_for_sections(test_input_file: str) -> int:
+    test_input = RFF.read_file_with_new_line(test_input_file)
+    priority_sum = 0
+    for backpack in test_input:
+        sectioner = len(backpack) // 2
+        first_half, second_half = backpack[:sectioner], backpack[sectioner:]
+        common_snack = HF.find_common_element([first_half, second_half])
+        priority_sum += get_priority_sum(common_snack[0])
+    return priority_sum
+
+
+def calculate_priority_sum_for_elf_groups(test_input_file: str) -> int:
+    test_input = RFF.read_file_with_new_line(test_input_file)
+    priority_sum = 0
+    for i in range(0, len(test_input), 3):
+        first_half, second_elf, third_elf = test_input[i], test_input[i+1], test_input[i+2]
+        common_snack = HF.find_common_element([first_half, second_elf, third_elf])
+        priority_sum += get_priority_sum(common_snack[0])
+    return priority_sum
+
+
+if __name__ == '__main__':
+    backpack_priority_sum = calculate_priority_sum_for_sections("backpack_snack_info.txt")
+    print(backpack_priority_sum)
+
+    group_priority_sum = calculate_priority_sum_for_elf_groups("backpack_snack_info.txt")
+    print(group_priority_sum)
--- day_3/main_before_clean_up.py
+++ day_3/main_before_clean_up.py
@@ -12,9 +12,11 @@ def test_function(test_input_file):
         section_1 = sorted(section_1)
         section_2 = sorted(section_2)
 
-        while (section_1 != []) or (section_2 != []):
+        # using two stacks to find the common char in two sorted lists
+        while (section_1 != []) and (section_2 != []):
             if section_1[0] == section_2[0]:
-                if ord(section_1[0]) < 97: # Uppercase
+                # uppercase
+                if ord(section_1[0]) < 97:
                     total_count += ord(section_1[0]) - 38
                 else:
                     total_count += ord(section_1[0]) - 96
@@ -44,6 +46,8 @@ def part_2_function(test_input_file):
         second_elf = list(dict.fromkeys(second_elf))
         third_elf = list(dict.fromkeys(third_elf))
 
+        # similar as above but check the first two elves first
+        # find the common chars between the two of them and store them in a list
         while (first_elf != []) and (second_elf != []):
             if first_elf[0] == second_elf[0]:
                 common_ground.append(first_elf[0])
@@ -54,12 +58,11 @@ def part_2_function(test_input_file):
             else:
                 second_elf.pop(0)
 
-        # print(common_ground)
-        # print(third_elf)
-
-        while (common_ground != []) or (third_elf != []):
+        # find the common char between the common list and the third elf
+        while (common_ground != []) and (third_elf != []):
             if common_ground[0] == third_elf[0]:
-                if ord(common_ground[0]) < 97: # Uppercase
+                # uppercase
+                if ord(common_ground[0]) < 97:
                     total_count += ord(common_ground[0]) - 38
                 else:
                     total_count += ord(common_ground[0]) - 96
@@ -72,11 +75,14 @@ def part_2_function(test_input_file):
     return total_count
 
 
-
 if __name__ == '__main__':
-    # count_total = test_function("real_input.txt")
-    # print(count_total)
+    count_total = test_function("backpack_snack_info.txt")
+    print(count_total)
 
-    part_2_ans = part_2_function("real_input.txt")
+    part_2_ans = part_2_function("backpack_snack_info.txt")
     print(part_2_ans)
+
+    # answers:
+    # 8085
+    # 2515
 
