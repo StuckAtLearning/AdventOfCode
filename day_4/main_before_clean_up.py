725011f58d4967c3d80052c885e95d402e0a350b
NoelleWeasley <NoelleWeasley@NoelleWeasley.local>
2022-12-04 16:51:37 -0500

    day_4: comments added

--- day_4/main_before_clean_up.py
+++ day_4/main_before_clean_up.py
@@ -8,15 +8,17 @@ def part_1(test_input_file):
 
     count = 0
     for single_input in test_input:
+        # parsing the input
         pairings = single_input.split(",")
         left = (pairings[0].split("-")[0], pairings[0].split("-")[1])
         right = (pairings[1].split("-")[0], pairings[1].split("-")[1])
 
+        # check if the left input fully engulf the right input
         if (int(left[0]) <= int(right[0])) and (int(left[1]) >= int(right[1])):
             count += 1
+        # check the other way around
         elif (int(left[0]) >= int(right[0])) and (int(left[1]) <= int(right[1])):
             count += 1
-        # elif(left[0] == right[0]) and (left[2] == right[2])
 
     return count
 
@@ -29,6 +31,7 @@ def part_2(test_input_file):
         left = (int(pairings[0].split("-")[0]), int(pairings[0].split("-")[1]))
         right = (int(pairings[1].split("-")[0]), int(pairings[1].split("-")[1]))
 
+        # using sets to check overlaps
         left_set = set(range(left[0], left[1]+1))
         right_set = set(range(right[0], right[1]+1))
 
@@ -39,9 +42,13 @@ def part_2(test_input_file):
 
 
 if __name__ == '__main__':
-    print(part_1("test_input.txt"))
+    # print(part_1("test_input.txt"))
     print(part_1("real_input.txt"))
 
-    print(part_2("test_input.txt"))
+    # print(part_2("test_input.txt"))
     print(part_2("real_input.txt"))
+
+    # answers:
+    # 496
+    # 847
 
