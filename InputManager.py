c46a5a7979eaa7cabf5af9cc8ca04258d1d3536f
SuckAtLearning <suckatlearning@gmail.com>
2023-01-31 17:09:50 -0500

    basic input reader added

---
+++ InputManager.py
@@ -0,0 +1,16 @@
+def read_file_with_double_new_line(file_name: str) -> list[list[str]]:
+    with open(file_name, 'r', encoding='utf-8') as file:
+        data = file.read().split("\n\n")
+    data_list = [i.split("\n") for i in data]
+
+    file.close()
+    return data_list
+
+
+def read_file_with_new_line(file_name: str) -> list[str]:
+    with open(file_name, 'r', encoding='utf-8') as file:
+        data = file.read().split("\n")
+
+    file.close()
+    return data
+
