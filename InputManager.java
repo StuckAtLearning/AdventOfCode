afe78e7ea712220b9a21243d2075efba81a656dc
SuckAtLearning <suckatlearning@gmail.com>
2023-02-04 20:42:00 -0500

    to futher prove that I am indeed a masochist(?)

---
+++ InputManager.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class InputManager {
+    public static void main(String[] args) {
+        try {
+            File aocInput = new File('inputs')
+        }
+        catch {
+
+        }
+    }
+}
