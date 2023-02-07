eae1fdf9a3bc6c63c869c2e002cbf19694095772
SuckAtLearning <suckatlearning@gmail.com>
2023-02-06 21:41:44 -0500

    I did Java!

--- InputManager.java
+++ InputManager.java
@@ -1,26 +1,43 @@
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class InputManager {
-    public static void main(Path inputFilePath) {
-        readFileWithSingleNewLine(inputFilePath);
-    }
 
-    public static void readFileWithSingleNewLine(Path filePath) {
+    public static List<String> readFileWithSingleNewLine(Path filePath) {
         try {
-            File aocInput = new File(String.valueOf(filePath));
-            Scanner aocInputReader = new Scanner(aocInput);
-            while (aocInputReader.hasNextLine()) {
-                String input_data = aocInputReader.nextLine();
-                System.out.println(input_data);
-            }
-            aocInputReader.close();
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("File is not found.");
+            return Files.readAllLines(filePath);
+
+        } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException(e);
         }
+
     }
 
+
+    public static List<List<String>> readFileWithDoubleNewLine(Path filePath) {
+        try {
+            List<String> input_lines = Files.readAllLines(filePath);
+            List<List<String>> grouped_input_lines = new ArrayList<>();
+            List<String> group = new ArrayList<>();
+            for (String input_line : input_lines) {
+                if (input_line.equals("")) {
+                    grouped_input_lines.add(group);
+                    group = new ArrayList<>();
+                } else {
+                    group.add(input_line);
+                }
+            }
+            return grouped_input_lines;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 }
