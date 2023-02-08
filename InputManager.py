57c094e07f9b06b10a6aac5f50010269d8b56e1f
SuckAtLearning <suckatlearning@gmail.com>
2023-02-07 21:59:30 -0500

    combine read file functions with reading single and double line features

--- InputManager.py
+++ InputManager.py
@@ -1,35 +1,34 @@
 import re
-from typing import Tuple, Set
 
 
-def read_file_with_double_new_line(file_name: str, parse_int: bool = True) -> list[list[str | int]]:
-    with open(file_name, 'r', encoding='utf-8') as file:
-        data = file.read().split("\n\n")
-    if parse_int:
-        data_list = list()
-        for i in data:
-            input_line = i.split('\n')
-            grouped_data = list()
-            for j in input_line:
-                if j.isdigit():
-                    grouped_data.append(int(j))
-                else:
-                    int_input_line = parse_int_in_line(j)
-                    grouped_data.append(int_input_line)
-            data_list.append(grouped_data)
+def read_file(file_name: str, parse_int: bool = True, double_new_line: bool = False) -> \
+        list[str] | list[list[str | int]]:
+    if double_new_line:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            data = file.read().split("\n\n")
+        if parse_int:
+            data_list = list()
+            for i in data:
+                input_line = i.split('\n')
+                grouped_data = list()
+                for j in input_line:
+                    if j.isdigit():
+                        grouped_data.append(int(j))
+                    else:
+                        int_input_line = parse_int_in_line(j)
+                        grouped_data.append(int_input_line)
+                data_list.append(grouped_data)
+        else:
+            data_list = [i.split("\n") for i in data]
+        file.close()
+        return data_list
     else:
-        data_list = [i.split("\n") for i in data]
-    file.close()
-    return data_list
-
-
-def read_file_with_single_new_line(file_name: str, parse_int: bool = True) -> list[str]:
-    with open(file_name, 'r', encoding='utf-8') as file:
-        data_list = file.read().split("\n")
-    if parse_int:
-        data_list = [int(i) if i.isdigit() else parse_int_in_line(i) for i in data_list]
-    file.close()
-    return data_list
+        with open(file_name, 'r', encoding='utf-8') as file:
+            data_list = file.read().split("\n")
+        if parse_int:
+            data_list = [int(i) if i.isdigit() else parse_int_in_line(i) for i in data_list]
+        file.close()
+        return data_list
 
 
 def parse_int_in_line(input_line: str) -> list[int]:
--- tests/TestInputManager.py
+++ tests/TestInputManager.py
@@ -2,33 +2,41 @@ import pytest
 import InputManager as ir
 
 
-def test_read_file_with_double_new_line():
-    output_int = ir.read_file_with_double_new_line(
-        '../test_inputs/input_manager_test_inputs/double_new_line_int_only.txt')
-    assert output_int == [[9686, 10178, 3375, 9638], [24919, 15983, 18045], [24872, 35761]]
+def test_read_file():
+    output_int_double_new_line = ir.read_file(
+        '../test_inputs/input_manager_test_inputs/double_new_line_int_only.txt', double_new_line=True)
+    assert output_int_double_new_line == [[9686, 10178, 3375, 9638], [24919, 15983, 18045], [24872, 35761]]
 
-    output_str = ir.read_file_with_double_new_line(
-        '../test_inputs/input_manager_test_inputs/double_new_line_int_only.txt', parse_int=False)
-    assert output_str == [['9686', '10178', '3375', '9638'], ['24919', '15983', '18045'], ['24872', '35761']]
+    output_str_double_new_line = ir.read_file(
+        '../test_inputs/input_manager_test_inputs/double_new_line_int_only.txt', parse_int=False, double_new_line=True)
+    assert output_str_double_new_line == [['9686', '10178', '3375', '9638'],
+                                          ['24919', '15983', '18045'],
+                                          ['24872', '35761']]
 
-    output_mixed_int = ir.read_file_with_double_new_line(
-        '../test_inputs/input_manager_test_inputs/double_new_line_mixed_type.txt')
-    assert output_mixed_int == [[[9686], [10178], [3375], [9638]],
-                                [[24919, 68435], [1598, 76876], [79879, 18045, 54653]],
-                                [[24872, 786859, 767654], [35761, 90432]]]
+    output_mixed_int_double_new_line = ir.read_file(
+        '../test_inputs/input_manager_test_inputs/double_new_line_mixed_type.txt', double_new_line=True)
+    assert output_mixed_int_double_new_line == [[[9686], [10178], [3375], [9638]],
+                                                [[24919, 68435], [1598, 76876], [79879, 18045, 54653]],
+                                                [[24872, 786859, 767654], [35761, 90432]]]
 
-
-def test_read_file_with_single_new_line():
-    output_int = ir.read_file_with_single_new_line(
+    output_int_single_new_line = ir.read_file(
         '../test_inputs/input_manager_test_inputs/single_new_line_int_only.txt')
-    assert output_int == [9686, 10178, 3375, 9638, 24919, 15983, 18045, 24872, 35761]
-    output_str = ir.read_file_with_single_new_line(
+    assert output_int_single_new_line == [9686, 10178, 3375, 9638, 24919, 15983, 18045, 24872, 35761]
+
+    output_str_single_new_line = ir.read_file(
         '../test_inputs/input_manager_test_inputs/single_new_line_int_only.txt', parse_int=False)
-    assert output_str == ['9686', '10178', '3375', '9638', '24919', '15983', '18045', '24872', '35761']
-    output_mixed_int = ir.read_file_with_single_new_line(
+    assert output_str_single_new_line == ['9686', '10178', '3375', '9638', '24919', '15983', '18045', '24872', '35761']
+
+    output_mixed_int_single_new_line = ir.read_file(
         '../test_inputs/input_manager_test_inputs/single_new_line_mixed_type.txt')
-    assert output_mixed_int == [[9686], [10178], [3375], [9638], [24919, 68435], [1598, 76876], [79879, 18045, 54653],
-                                [24872, 786859, 767654], [35761, 90432]]
+    assert output_mixed_int_single_new_line == [[9686],
+                                                [10178],
+                                                [3375],
+                                                [9638],
+                                                [24919, 68435],
+                                                [1598, 76876],
+                                                [79879, 18045, 54653],
+                                                [24872, 786859, 767654], [35761, 90432]]
 
 
 def test_parse_int_in_line():
