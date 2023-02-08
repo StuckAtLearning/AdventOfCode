111240186e2c01f13c6d304e32f17b6b8c97dd1b
SuckAtLearning <suckatlearning@gmail.com>
2023-02-07 22:00:28 -0500

    update the java version of InputManager for better and cleaner solutions

--- InputManager.java
+++ InputManager.java
@@ -1,14 +1,11 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class InputManager {
-
-    public static List<String> readFileWithSingleNewLine(Path filePath) {
+    public static List<String> readFile(Path filePath) {
         try {
             return Files.readAllLines(filePath);
 
@@ -16,28 +13,21 @@ public class InputManager {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
-
     }
 
+    public static List<List<String>> readFile(Path filePath, boolean doubleNewLine) {
+        if (doubleNewLine) {
+            try {
+                return Arrays.stream(Files.readString(filePath).split("\n\n"))
+                        .map(x -> Arrays.stream(x.split("\n")).toList()).toList();
 
-    public static List<List<String>> readFileWithDoubleNewLine(Path filePath) {
-        try {
-            List<String> input_lines = Files.readAllLines(filePath);
-            List<List<String>> grouped_input_lines = new ArrayList<>();
-            List<String> group = new ArrayList<>();
-            for (String input_line : input_lines) {
-                if (input_line.equals("")) {
-                    grouped_input_lines.add(group);
-                    group = new ArrayList<>();
-                } else {
-                    group.add(input_line);
-                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
             }
-            return grouped_input_lines;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+        }
+        else {
+            return null;
         }
     }
 }
