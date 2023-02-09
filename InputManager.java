d1a1960fc8f06559b6e5e8879df00972c62ff5ea
SuckAtLearning <suckatlearning@gmail.com>
2023-02-08 22:17:04 -0500

    improved `InputManager`, because I guess it always needs improvements. My code is that bad.

--- InputManager.java
+++ InputManager.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputManager {
     public static List<String> readFile(Path filePath) {
@@ -15,19 +18,33 @@ public class InputManager {
         }
     }
 
-    public static List<List<String>> readFile(Path filePath, boolean doubleNewLine) {
-        if (doubleNewLine) {
-            try {
-                return Arrays.stream(Files.readString(filePath).split("\n\n"))
-                        .map(x -> Arrays.stream(x.split("\n")).toList()).toList();
 
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
+    public static List<List<String>> groupFileWithDoubleNewLine(List<String> fileInfo) {
+        List<List<String>> groupedInfo = new ArrayList<>();
+        List<String> group = new ArrayList<>();
+        for (String info : fileInfo) {
+            if (info.isEmpty()) {
+                groupedInfo.add(group);
+                group = new ArrayList<>();
+            } else {
+                group.add(info);
             }
         }
-        else {
-            return null;
+        groupedInfo.add(group);
+        return groupedInfo;
+    }
+
+
+    public static List<Integer> parseIntInLine(String info, boolean includeNegatives) {
+        Pattern intPattern;
+        if (includeNegatives) {
+            intPattern = Pattern.compile("-?\\d+");
         }
+        else {
+            intPattern = Pattern.compile("\\d+");
+        }
+        Matcher inputInfo = intPattern.matcher(info);
+        List<String> parsedInfo = inputInfo.results().map(MatchResult::group).toList();
+        return parsedInfo.stream().map(Integer::valueOf).toList();
     }
 }
