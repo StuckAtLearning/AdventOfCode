1a6307cecbf54760847dedcc3154dd96bc366501
SuckAtLearning <suckatlearning@gmail.com>
2023-02-22 22:16:28 -0500

    minor changes

--- tests/TestInputManager.py
+++ tests/TestInputManager.py
@@ -20,10 +20,10 @@ def test_group_file_info_with_single_new_line():
 
 def test_parse_int_in_line():
     output_positive = ir.parse_int_in_line(' : 3249 weruhuiqwre723491'' \ 3uh2h4 whiuh83279 32487692.34')
-    assert output_positive == [3249, 723491, 3, 2, 4, 83279, 32487692, 34]
+    assert output_positive == (3249, 723491, 3, 2, 4, 83279, 32487692, 34)
 
     output_negative = ir.parse_int_in_line(' : -3249 weruhuiqwre-723491'' \ 3uh-2h4 whiuh83279 32487692.34', True)
-    assert output_negative == [-3249, -723491, 3, -2, 4, 83279, 32487692, 34]
+    assert output_negative == (-3249, -723491, 3, -2, 4, 83279, 32487692, 34)
 
 
 def test_parse_grid():
